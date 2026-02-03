f1312df778e264a38452a7f349f437fb2f8ff655
Mikke Schirén <mikke.schiren@digitalist.com>
2026-02-03 23:51:09 +0100

    remove dumping of users

--- Commands/KeyCloakSync.php
+++ Commands/KeyCloakSync.php
@@ -62,7 +62,6 @@ class KeyCloakSync extends ConsoleCommand
 
         try {
             $users = $this->getUsers($baseUrl, $realm, $client, $secret);
-            var_dump($users);
             $output->writeln(sprintf('<info>Fetched %d users from Keycloak.</info>', count($users)));
         } catch (Exception $e) {
             $output->writeln('<error>Failed to fetch users from Keycloak: ' . $e->getMessage() . '</error>');
